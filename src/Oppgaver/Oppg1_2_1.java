50caf4751235bcd39613249a3fa1fa088ec7b349
gautte <s362066@oslomet.no>
2022-08-29 23:15:14 +0200

    Oppg 1.2.5 og lagt til main for å sjekke oppg 1.2.5

--- src/Oppgaver/Oppg1_2_1.java
+++ src/Oppgaver/Oppg1_2_1.java
@@ -1,6 +1,12 @@
 package Oppgaver;
 
+import java.util.Arrays;
+
 public class Oppg1_2_1 {
+    public static void main(String[] args) {
+        int[]a = {1,2,3,4,5,6};
+        System.out.println(forAlle(a));
+    }
     public static int min(int[] a, int fra, int til) {
         if (fra < 0 || til > a.length || fra >= til) {
             throw new IllegalArgumentException("Illegalt intervall!");
@@ -21,4 +27,17 @@ public class Oppg1_2_1 {
     public static int min(int[] a){
         return min(a,0,a.length);
     }
+
+    public static int forAlle(int[] a){
+        int[] b = Arrays.copyOfRange(a,3,5);
+        int heltallEks = 0;
+        for(int n : b){
+             heltallEks += n;
+         }
+        return heltallEks;
+    }
+    //1.2.5
 }
+
+
+
