0b170df8f9d7d6d4e6ebccaa079b0b3f6e9cf5c5
gautte <s362066@oslomet.no>
2022-09-05 15:16:18 +0200

    Oppgave 1.3.4 - 5)

--- src/uke3/Oppg1_3_4.java
+++ src/uke3/Oppg1_3_4.java
@@ -11,4 +11,22 @@ public class Oppg1_3_4 {
         Tabell.snu(a);                        // tabellen snus
         Tabell.skriv(a);                      // 10 9 8 7 6 5 4 3 2 1
     }
+    public static void selectionSort(int[] a){
+
+        for(int i = 0; i < a.length; i++) {
+            int m = i;                              // indeks til den foreløpig minste
+            int minVerdi = a[i];                    // verdien til den foreløpig minste
+
+            for(int j = 0; j < a.length; < j++) {
+
+                if(a[j] < minVerdi) {
+                    minVerdi = a[j];                // ny minste verdi
+                    m = j;                          // indeks til ny minste verdi
+                }
+            }
+            // Bytter om a[i] og a[m]
+            a[m] = a[i];
+            a[i]; minVerdi;
+        }
+    }
 }
