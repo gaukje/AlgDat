4552176a5c7bace4e06038cba0fabe59f804a816
gautte <s362066@oslomet.no>
2022-09-27 12:13:58 +0200

    Fullført oppgaver til 3.2.2

---
+++ src/uke6/Oppg3_2_2.java
@@ -0,0 +1,18 @@
+package uke6;
+
+import hjelpeklasser.Liste;
+import hjelpeklasser.TabellListe;
+
+public class Oppg3_2_2 {
+    public static void main(String[] args) {
+        //3.2.2 - oppg. 2
+        String[] s = {"Per", "Kari", "Ole", "Azra", "", "", "leila"};
+        Liste<String> liste = new TabellListe<>(s);
+        System.out.println(liste.hent(3));
+        System.out.println(liste.inneholder("Ole"));
+        System.out.println(liste.indeksTil(""));
+
+        //Oppgave 3)
+        System.out.println(liste);
+    }
+}
