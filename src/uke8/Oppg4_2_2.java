698e03b19e69317141b6a0c95c143121b2291cf3
gautte <s362066@oslomet.no>
2022-10-13 13:29:06 +0200

    Begynt på oppgaver til 4.2.2 - En sirkulær kø - de 3 første ble gjort i skriveboka til matte

---
+++ src/uke8/Oppg4_2_2.java
@@ -0,0 +1,11 @@
+package uke8;
+
+import hjelpeklasser.Kø;
+import hjelpeklasser.TabellKø;
+
+public class Oppg4_2_2 {
+    public static void main(String[] args) {
+        Kø<Character> a = new TabellKø<>(16);
+
+    }
+}
