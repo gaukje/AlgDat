9e0d178c7e1259aec68b16fbffb81020ceecd534
gautte <s362066@oslomet.no>
2022-10-28 10:03:19 +0200

    5.2.2 - SBinTre

---
+++ src/hjelpeklasser/SBinTre.java
@@ -0,0 +1,46 @@
+package hjelpeklasser;
+
+import java.util.Comparator;
+
+public class SBinTre<T> // implements Beholder<T>
+{
+    private static final class Node<T> // en indre nodeklasse
+    {
+        private T verdi;                 // nodens verdi
+        private Node<T> venstre, høyre;  // venstre og høyre barn
+
+        private Node(T verdi, Node<T> v, Node<T> h)  // konstruktør
+        {
+            this.verdi = verdi;
+            venstre = v;
+            høyre = h;
+        }
+
+        private Node(T verdi)  // konstruktør
+        {
+            this(verdi, null, null);
+        }
+    } // class Node
+
+    private Node<T> rot;                       // refranse til rotnoden
+    private int antall;                        // antall noder
+    private final Comparator<? super T> comp;  // komparator
+
+    public SBinTre(Comparator<? super T> c)    // konstruktør
+    {
+        rot = null;
+        antall = 0;
+        comp = c;
+    }
+
+    public int antall()        // antall verdier i treet
+    {
+        return antall;
+    }
+
+    public boolean tom()       // er treet tomt?
+    {
+        return antall == 0;
+    }
+
+} // class SBinTre
